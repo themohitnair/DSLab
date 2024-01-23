c8929008d1a361ed31f35a01fe3c59505675a503
Mohit Nair <mohitnair051204@gmail.com>
2024-01-23 18:05:52 +0530

    New Singly Linked List program

--- program8.c
+++ program8.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
-#include<malloc.h>
 #include<string.h>
+#include<malloc.h>
+#include<stdbool.h>
 
 struct node {
     char data;
     struct node* link;
 };
 
-bool empty(struct node* head)
-{
-    return (head->link == NULL);
-}
-
 struct node* create()
 {
-    struct node* head = (struct node*)malloc(sizeof(struct node*));
+    struct node* head = (struct node*)malloc(sizeof(struct node));
     head->data = '-';
     head->link = NULL;
     return head;
+}
+
+bool empty(struct node* head)
+{
+    return head->link == NULL;
+}
+
+void insert_head(struct node* head, char item)
+{
+    struct node* ptr = head;
+    struct node* next = head->link;
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = item;
+    newnode->link = next;
+    head->link = newnode;
 }
 
 void insert_tail(struct node* head, char item)
@@ -32,60 +42,176 @@ void insert_tail(struct node* head, char item)
         else
             ptr = ptr->link;
     }
-    struct node* newnode = (struct node*)malloc(sizeof(struct node*));
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
     newnode->data = item;
-    ptr->link = newnode;
     newnode->link = NULL;
+    ptr->link = newnode;
 }
 
-void insert_head(struct node* head, char item)
+void insert_pos(struct node* head, char item, int pos)
 {
     struct node* ptr = head;
-    struct node* right = head->link;
-    ptr->link = right;
-    struct node* newnode = (struct node*)malloc(sizeof(struct node*));
-    newnode->data = item;
-    newnode->link = right;
-    head->link = newnode;    
+    int length = 0;
+    while(true)
+    {
+        if(ptr->link==NULL)
+            break;
+        else
+        {
+            length++;
+            ptr = ptr->link;
+        }
+    }
+    if(pos>length)
+    {
+        printf("Position out of bounds.");
+        exit(0);
+    }
+    else if(pos == length)
+    {
+        insert_tail(head, item);
+        return;
+    }
+    else
+    {
+        ptr = head;
+        while(pos > 0)    
+        {
+            ptr = ptr->link;
+            pos--;       
+        }
+        struct node* next = ptr->link;
+        struct node* newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = item;
+        ptr->link = newnode;
+        newnode->link = next;
+    }
 }
 
 char delete_head(struct node* head)
 {
-    struct node* ptr = head;
-    if(ptr->link==NULL)
+    if(empty(head))
     {
-        printf("List empty. No items left to delete. ");
+        printf("No items left to be deleted. List is empty.");
         exit(0);
     }
-
-    struct node* newfirst = ptr->link->link;
-    char deleted = ptr->link->data;
-    free(ptr->link);
+    struct node* ptr = head;
+    struct node* del = head->link;
+    struct node* newfirst = del->link;
     ptr->link = newfirst;
+    char deleted = del->data;
+    free(del);
     return deleted;
 }
 
 char delete_tail(struct node* head)
 {
-    struct node* ptr = head;
-    if(ptr->link == NULL)
+    if(empty(head))
     {
-        printf("List empty. No items left to delete. ");
+        printf("No items left to be deleted. List is empty.");
         exit(0);
     }
-    while(ptr->link->link != NULL)
+    struct node* ptr = head;
+    while(true)
     {
-        ptr = ptr->link;
+        if(ptr->link->link == NULL)
+            break;
+        else
+            ptr = ptr->link;
     }
-    char deleted = ptr->link->data;
+    struct node* del = ptr->link;
     ptr->link = NULL;
+    char deleted = del->data;
+    free(del);
     return deleted;
+}
+
+char delete_pos(struct node* head, int pos)
+{
+    if(empty(head))
+    {
+        printf("No items left to be deleted. List is empty.");
+        exit(0);
+    }
+    struct node* ptr = head;
+    int length = 0;
+    while(true)
+    {
+        if(ptr->link==NULL)
+            break;
+        else
+        {
+            length++;
+            ptr = ptr->link;
+        }
+    }
+    if(pos>length)
+    {
+        printf("Position out of bounds.");
+        exit(0);
+    }
+    else if(pos==length)
+    {
+        return delete_tail(head);
+    }
+    else
+    {
+        struct node* ptr = head;
+        pos--;
+        while(pos > 0)
+        {
+            ptr = ptr->link;
+            pos--;        
+        }
+        struct node* del = ptr->link;
+        struct node* next = del->link;
+        ptr->link = next;
+        char deleted = del->data;
+        free(del);
+        return deleted;
+    }
+}
+
+int search(struct node* head, char item)
+{
+    if(empty(head))
+    {
+        printf("No items left to be searched from. List is empty.");
+        exit(0);
+    }
+    struct node* ptr = head;    
+    int flag = 0;
+    int count = 0;
+    while(true)
+    {
+        if(ptr->data == item)
+        {
+            flag = 1;
+            break;
+        }
+        else
+        {
+            count++;
+            ptr = ptr->link;
+        }
+        if(ptr==NULL)
+            break;
+    }
+    if(flag)
+        return count;
+    else
+        return -1;
 }
 
 void display(struct node* head)
 {
-    struct node* ptr = head;
-    printf("\nThe contents of the list are: ");
+    if(empty(head))
+    {
+        printf("No items left to be displayed. List is empty.");
+        exit(0);
+    }
+    struct node* ptr = head->link;
+    printf("The contents of the list are: ");
     while(ptr!=NULL)
     {
         printf("%c",ptr->data);
@@ -94,103 +220,117 @@ void display(struct node* head)
     printf("\n");
 }
 
+char get_item()
+{
+    char item;
+    printf("Enter the item: ");
+    scanf(" %c",&item);
+    return item;
+}
+
+int get_pos()
+{
+    int pos;
+    printf("Enter the position: ");
+    scanf("%d",&pos);
+    return pos;
+}
+
 void liberate(struct node* head)
 {
-    struct node* ptr = head;
-    while(ptr!=NULL)
-    {
-        free(ptr);
-        ptr = ptr->link;
-    }
+    
 }
 
 int main(void)
 {
     struct node* head = create();
     int choice;
-    char item;
-    printf("1. Insert at tail\n2. Insert at head\n3. Delete at tail\n4. Delete at head\n5. Display\n6. Exit\n");
+    printf("1. Insert at tail\n2. Insert at head\n3. Insert after position\n4. Delete at tail\n5. Delete at head\n6. Delete at position\n7. Search an element\n8. Display\n9. Exit\n");
     while(true)
     {
         printf("Enter your choice: ");
         scanf("%d",&choice);
         switch(choice)
         {
-            case 1: 
-            printf("Enter the item to be inserted: ");
-            scanf(" %c",&item);
-            insert_tail(head,item);
+            case 1:
+            insert_tail(head,get_item());
             break;
+
             case 2:
-            printf("Enter the item to be inserted: ");
-            scanf(" %c",&item);
-            insert_head(head,item);
+            insert_head(head,get_item());
             break;
+
             case 3:
+            insert_pos(head, get_item(), get_pos());
+            break;
+
+            case 4:
             printf("Deleted element: %c\n",delete_tail(head));
             break;
-            case 4:
+
+            case 5:
             printf("Deleted element: %c\n",delete_head(head));
             break;
-            case 5:
+
+            case 6:
+            printf("Deleted element: %c\n",delete_pos(head, get_pos()));
+            break;
+
+            case 7:
+            printf("Element is found at node %d of the list.\n",search(head, get_item()));
+            break;
+
+            case 8:
             display(head);
             break;
-            case 6:
+
+            case 9:
             exit(0);
+
             default:
             printf("Invalid choice. Please choose again.");
         }
     }
-    liberate(head);
 }
 
 /*Output:
 1. Insert at tail
 2. Insert at head
-3. Delete at tail
-4. Delete at head
-5. Display
-6. Exit
+3. Insert after position
+4. Delete at tail
+5. Delete at head
+6. Delete at position
+7. Search an element
+8. Display
+9. Exit
 Enter your choice: 1
-Enter the item to be inserted: a
+Enter the item: a
 Enter your choice: 1
-Enter the item to be inserted: b
+Enter the item: b
 Enter your choice: 1
-Enter the item to be inserted: d
-Enter your choice: 1
-Enter the item to be inserted: k
-Enter your choice: 1
-Enter the item to be inserted: o
-Enter your choice: 1
-Enter the item to be inserted: h
-Enter your choice: 1
-Enter the item to be inserted: l
-Enter your choice: 1
-Enter the item to be inserted: i
+Enter the item: c
+Enter your choice: 2
+Enter the item: 1
+Enter your choice: 2
+Enter the item: 2
+Enter your choice: 2
+Enter the item: 3
+Enter your choice: 3
+Enter the position: 3
+Enter the item: 0
+Enter your choice: 8
+The contents of the list are: 3210abc
+Enter your choice: 4
+Deleted element: c
 Enter your choice: 5
-
-The contents of the list are: -abdkohli
-Enter your choice: 4
-Deleted element: a
-Enter your choice: 4
+Deleted element: 3
+Enter your choice: 6
+Enter the position: 5
 Deleted element: b
-Enter your choice: 4
-Deleted element: d
-Enter your choice: 5
-
-The contents of the list are: -kohli
-Enter your choice: 3
-Deleted element: i
-Enter your choice: 3
-Deleted element: l
-Enter your choice: 5
-
-The contents of the list are: -koh
-Enter your choice: 4
-Deleted element: k
-Enter your choice: 4
-Deleted element: o
-Enter your choice: 4
-Deleted element: h
-Enter your choice: 4
-List empty. No items left to delete.*/
+Enter your choice: 8
+The contents of the list are: 210a
+Enter your choice: 7
+Enter the item: 0
+Element is found at node 3 of the list.
+Enter your choice: 9
+*/
